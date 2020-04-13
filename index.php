55401b3fa6858e4915d0dfbae7f8200d86b12e9d
anggitarisnawati <anggitrsnwati@gmail.com>
2020-04-14 06:46:11 +0700

    nim, nama, alamat

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	echo "Anggita, akan ada sedikit perubahan....";
-	echo "Anggita, akan ada sedikit perubahan....";
-	echo "Anggita, akan ada sedikit perubahan....";
-	echo "Anggita, akan ada sedikit perubahan....";
+	echo "Tugas Praktikum TCC ke 8";
+	echo "Nama   : Anggita Risnawati";
+	echo "NIM    : 165610053";
+	echo "Alamat : Condoncatur, Karangasem, Depok, Sleman, Yogyakarta";
 ?>
